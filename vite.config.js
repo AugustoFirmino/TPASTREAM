208352348e1e5250a6aea84bf1b9e11078c3fab9
Augusto Firmino <gugufirmino1l@gmail.com>
2026-07-12 18:54:50 +0100

    Configurar socket publico para founder agora

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,31 @@ export default defineConfig({
 
     host: "0.0.0.0",
 
-    port: 5713
+    port: 5713,
+
+  },
+
+
+  build: {
+
+    manifest: true,
+
+    sourcemap: false,
+
+    rollupOptions: {
+
+      output: {
+
+        // cria nomes diferentes quando o código muda
+        entryFileNames: "assets/[name]-[hash].js",
+
+        chunkFileNames: "assets/[name]-[hash].js",
+
+        assetFileNames: "assets/[name]-[hash][extname]"
+
+      }
+
+    }
 
   }
 
